afdeebb7c37e5cb88e6dd52f35ec8e9a59803909
Oleg S <ovsavchuk81@gmail.com>
2019-02-18 23:59:08 +0300

    for

--- BotPinProject/Program.cs
+++ BotPinProject/Program.cs
@@ -23,270 +23,44 @@ namespace BotPin
         {
             //CreateXML();
             //ClearXML();
-            var r = new Rubricator(AppDomain.CurrentDomain.BaseDirectory, Resources.GoToUrl);
 
-            int i = 6;
-            while (i > 0)
+            int m = 6;
+            while (m > 0)
             {
-                if ((int)r.chekState == 999) break;
-                r.SignIn(Resources.nickname, Resources.psw, 1000);
-                r.Subscription(1000);
-                r.ClickButtonAddPin(1000);
-                r.ClickButtonFromInternet(1000);
-                r.GetRendomAttributte();
-                r.SetURL(1000);
-                r.ClickButtonFind(1000);
-                r.SetPicture(1000);
-                r.SelectCollection(1000);
-                r.SetDescription(1000);
-                r.ClickButtonChoosePicture(1000);
-                r.ClickButtonSave(1000);
-                i--;
+
+                var r = new Rubricator(AppDomain.CurrentDomain.BaseDirectory, Resources.GoToUrl);
+
+                int i = 6;
+                while (i > 0)
+                {
+                    if ((int)r.chekState == 999) break;
+                    r.SignIn(Resources.nickname, Resources.psw, 1000);
+                    r.Subscription(1000);
+                    r.ClickButtonAddPin(1000);
+                    r.ClickButtonFromInternet(1000);
+                    r.GetRendomAttributte();
+                    r.SetURL(1000);
+                    r.ClickButtonFind(1000);
+                    r.SetPicture(1000);
+                    r.SelectCollection(1000);
+                    r.SetDescription(1000);
+                    r.ClickButtonChoosePicture(1000);
+                    r.ClickButtonSave(1000);
+                    i--;
+                }
+
+                sl.Sleep(10000);
+
+                if ((int)r.chekState == 999)
+                { r.Close(1000); break; }
+                r.Close(1000);
+                m--;
             }
-                       
-            sl.Sleep(10000);
-            r.Close(1000);
+
             Environment.Exit(0);
 
         }
 
-        /*
-        static int BotGo(int t)
-        {
-           //Rubricator rb = new Rubricator(driver, AppDomain.CurrentDomain.BaseDirectory);
-           
-
-            if (t == 5)
-            {
-                return 5;
-            }
-            else
-            {
-                logger.Debug("Попытка № {0}", t + 1);
-                try
-                {
-
-                    //логинемся
-                    Login(AppDomain.CurrentDomain.BaseDirectory);
-
-                    IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-
-
-                    //закрываю подписку, если предлагает               
-                    try
-                    {
-                        wait.Until(SCond.ElementExists(By.Id("sysNotifyInvitePopup")));
-                        js.ExecuteScript("$('#sysNotifyInvitePopup').dialog('close'); return false;");
-                        logger.Debug("log {0}", "закрываю подписку");
-                    }
-                    catch (Exception)
-                    {
-                        logger.Debug("log {0}", "Подписки нет");
-                    }
-
-
-
-                    //добавляю pin
-                    wait.Until(SCond.ElementExists(By.ClassName("AddIcon")));
-                    js.ExecuteScript("PinCreateLoader.open()");
-                    logger.Debug("log {0}", "Нажата кнопка 'Добавить пин'");
-
-
-                    //из интернета
-                    //wait.Until((x) =>
-                    //{
-                    //    return x.FindElement(By.Id("sysPinCreatePopup")).Enabled;
-                    //});
-                    wait.Until(SCond.ElementExists(By.Id("sysPinCreatePopup")));
-                    //wait.Until(c => c.FindElement(By.Id("sysPinCreatePopup")).Enabled);
-                    js.ExecuteScript("PinCreate.open('add')");
-                    logger.Debug("log {0}", "Нажата кнопка 'Из интернета'");
-
-
-                    //произвольный пост
-                    XmlAttributeCollection attr = GetAttr();
-
-                    logger.Debug("log Получили атрибут с urltogo='{0}'", attr.GetNamedItem("urltogo").Value.ToString());
-                    logger.Debug("log Получили атрибут с urlpic='{0}'", attr.GetNamedItem("urlpic").Value.ToString());
-                    logger.Debug("log Получили атрибут с desc='{0}'", attr.GetNamedItem("desc").Value.ToString());
-
-
-
-                    //из интернета;
-                    wait.Until(SCond.ElementExists(By.Name("url")));
-                    List<IWebElement> linksToClickUrl = driver.FindElements(By.Name("url")).ToList();
-
-                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].setAttribute('class','vote-link up voted')", linksToClickUrl[linksToClickUrl.Count - 1]);
-                    linksToClickUrl[linksToClickUrl.Count - 1].SendKeys(attr.GetNamedItem("urltogo").Value.ToString());
-
-                    List<IWebElement> linksToClickFind = driver.FindElements(By.ClassName("wr_bordered_button")).ToList();
-                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].setAttribute('class','vote-link up voted')", linksToClickFind[linksToClickFind.Count - 1]);
-                    linksToClickFind[linksToClickFind.Count - 1].Click();
-                    logger.Debug("Начали поиск для url='{0}'", attr.GetNamedItem("urltogo").Value.ToString());
-
-
-                    //sl.Sleep(10000);
-
-                    wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-                    wait.Until((x) =>
-                    {
-                        return driver.FindElements(By.XPath("//img[@src='https://cdn-nus-1.pinme.ru/asset/rele/img/icons/uploading_3.gif']")).ToList().Count > 0;
-                    });
-
-                    List<IWebElement> linksloadImg = driver.FindElements(By.XPath("//img[@src='https://cdn-nus-1.pinme.ru/asset/rele/img/icons/uploading_3.gif']")).ToList();
-                    logger.Debug("Нашли картиноку uploading_3.gif");
-
-                    logger.Debug("Всего количество картинок {0}", linksloadImg.Count.ToString());
-                    sl.Sleep(1000);
-
-                    wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-                    wait.Until((x) =>
-                    {
-                        return !linksloadImg[linksloadImg.Count - 1].Displayed;
-                    });
-                    logger.Debug("Дождались загрузку картинок");
-
-
-                    //List<IWebElement> linksToClickimgWrap = driver.FindElements(By.ClassName("imgWrap")).ToList();
-                    //linksToClickFind[0].SendKeys("ssss");
-                    //driver.FindElements(By.ClassName("imgWrap")).ToList()[0].FindElement(;
-                    js.ExecuteScript(string.Format("document.getElementsByClassName('imgWrap')[0].getElementsByTagName('img')[0].setAttribute('src', '{0}')", attr.GetNamedItem("urlpic").Value.ToString()));
-                    sl.Sleep(1000);
-                    logger.Debug("Присвоили картинке urlpic='{0}'", attr.GetNamedItem("urlpic").Value.ToString());
-                    //driver.FindElement(By.CssSelector(".jcarousel-next.jcarousel-next-horizontal")).Click();
-
-                    //for (int i = 0; i <= linksToClickimgWrap.Count - 1; i++)
-                    //{
-                    //    if (IsElementExists(By.XPath(string.Format("img[@src='{0}']", attr.GetNamedItem("urlpic").Value.ToString())), linksToClickimgWrap[i]))
-                    //    {
-                    //        findFoto = true;
-                    //        break;
-                    //    }
-                    //    sl.Sleep(1000);
-                    //    driver.FindElement(By.CssSelector(".jcarousel-next.jcarousel-next-horizontal")).Click();
-
-                    //}
-                    //findFoto = true;
-
-                    //if (findFoto)
-                    //{
-                    sl.Sleep(1000);
-                    //wait.Until(ExpectedConditions.ElementExists(By.Id("sysForm_id_descr")));
-                    List<IWebElement> linksToClickDescr = driver.FindElements(By.Id("sysForm_id_descr")).ToList();
-                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].setAttribute('class','vote-link up voted')", linksToClickDescr[linksToClickDescr.Count - 1]);
-                    linksToClickDescr[linksToClickDescr.Count - 1].SendKeys(attr.GetNamedItem("desc").Value.ToString());
-                    logger.Debug("Присвоили описание desc='{0}'", attr.GetNamedItem("desc").Value.ToString());
-
-                    sl.Sleep(1000);
-                    //wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@value='Выбрать']")));
-                    List<IWebElement> linksToClickToChoise = driver.FindElements(By.XPath("//input[@value='Выбрать']")).ToList();
-                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].setAttribute('class','vote-link up voted')", linksToClickToChoise[linksToClickToChoise.Count - 1]);
-                    linksToClickToChoise[linksToClickToChoise.Count - 1].Click();
-                    logger.Debug("Нажали на кнопку 'Выбрать'");
-
-
-                    sl.Sleep(2000);
-                    //wait.Until(ExpectedConditions.ElementExists(By.XPath("//input[@value='Сохранить']")));
-                    List<IWebElement> linksToClickSave = driver.FindElements(By.XPath("//input[@value='Сохранить']")).ToList();
-                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].setAttribute('class','vote-link up voted')", linksToClickSave[linksToClickSave.Count - 1]);
-                    linksToClickSave[linksToClickSave.Count - 1].Click();
-                    logger.Debug("Нажали на кнопку 'Сохранить'");
-
-                    sl.Sleep(10000);
-                    //}
-                    //else
-                    //    Console.WriteLine(string.Format("битое фото в папке {0} для ссылки {1}]", attr.GetNamedItem("urltogo").Value.ToString(), attr.GetNamedItem("urlpic").Value.ToString()));
-
-
-                    logger.Debug("Успешно добавлено фото");
-                    driver.Close();
-                    driver.Dispose();
-                    return BotGo(5);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-
-                    logger.Debug("Не успешно добавлено фото");
-                    driver.Close();
-                    driver.Dispose();
-                    return BotGo(t + 1);
-                }
-
-
-            }
-        }
-
-
-
-        //static XmlAttributeCollection GetAttr()
-        //{
-
-        //    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-        //    XmlDocument xDoc = new XmlDocument();
-        //    xDoc.Load(AppDomain.CurrentDomain.BaseDirectory + Resources.xml);
-
-        //    // получим корневой элемент
-        //    XmlElement xRoot = xDoc.DocumentElement;
-        //    Random rnd = new Random();
-
-        //    return xRoot.ChildNodes[rnd.Next(0, xRoot.ChildNodes.Count)].Attributes;
-        //}
-
-        //static void Login(string chrome)
-        //{
-        //    driver = new ChromeDriver(chrome);
-        //    driver.Navigate().GoToUrl(Resources.GoToUrl);
-
-        //    logger.Debug("Перешли на сайт {0}", Resources.GoToUrl);
-
-        //    wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-        //    wait.Until(SCond.ElementExists(By.Name("nickname")));
-            
-        //    driver.FindElement(By.Name("nickname")).SendKeys(Resources.nickname);
-
-
-        //    wait.Until(SCond.ElementExists(By.Name("password")));
-            
-        //    driver.FindElement(By.Name("password")).SendKeys(Resources.psw);
-
-        //    driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(15);
-
-        //    try
-        //    {
-
-        //        logger.Debug("Пытаемся логиниться под {0}", Resources.nickname);
-        //        wait.Until(SCond.ElementExists(By.Id("sysForm_submit")));
-        //        driver.FindElement(By.Id("sysForm_submit")).Click();
-        //    }
-        //    catch (Exception ) { }
-
-        //    driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
-
-
-        //    //wait.Until((x) =>
-        //    //{
-        //    //    return driver.FindElements(By.ClassName("")).ToList().Count > 0;
-        //    //});
-
-            
-        //    //UserNav submenu submenu_hover
-        //}
-
-        //public static bool IsElementExists(By iClassName, IWebElement we)
-        //{
-        //    try
-        //    {
-        //        string i = we.FindElement(iClassName).Size.ToString();
-        //        return true;
-        //    }
-        //    catch (NoSuchElementException)
-        //    {
-        //        return false;
-        //    }
-        //}
-        */
         static void CreateXML()
         {
 
@@ -354,10 +128,10 @@ namespace BotPin
                 node.Attributes.GetNamedItem("urltogo").Value = attUrlGo.Replace("www.", "");
 
                 attDesc = node.Attributes.GetNamedItem("desc").Value.ToString();
-                node.Attributes.GetNamedItem("desc").Value = attDesc.Replace("2012", "");
-                node.Attributes.GetNamedItem("desc").Value = attDesc.Replace("2013", "");
-                node.Attributes.GetNamedItem("desc").Value = attDesc.Replace("2014", "");
-                node.Attributes.GetNamedItem("desc").Value = attDesc.Replace("2015", "");
+                attDesc = attDesc.Replace("2012", "");
+                attDesc = attDesc.Replace("2013", "");
+                attDesc = attDesc.Replace("2014", "");
+                attDesc = attDesc.Replace("2015", "");
                 node.Attributes.GetNamedItem("desc").Value = attDesc.Replace("2011", "");
 
                 if (Regex.IsMatch(node.Attributes.GetNamedItem("desc").Value.ToString().ToUpper(), "МУЖ"))
